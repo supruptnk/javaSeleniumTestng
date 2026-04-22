d7cef235ac1508276c5950ef8e32921254e033d8
Supragyan Pattanayak <supragyan@live.com>
2026-04-22 23:52:46 +0530

    added driver

---
+++ demo/src/main/java/practice/selenium/testng/Main.java
@@ -0,0 +1,11 @@
+package practice.selenium.testng;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        
+    }
+}
