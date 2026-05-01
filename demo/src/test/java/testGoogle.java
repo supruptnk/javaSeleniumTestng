7dc4a45eb8af79f035f88e5b485d885e86dc3fba
Supragyan Pattanayak <supragyan@live.com>
2026-05-02 00:05:40 +0530

    added test case

---
+++ demo/src/test/java/testGoogle.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+public class testGoogle extends BaseTest{
+
+    @Test
+    public void testGoogleSearch() {
+        System.out.println("hello");
+    }
+    
+}
